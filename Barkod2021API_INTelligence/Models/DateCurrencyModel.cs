b8ea55063ca9cad59b15039f136abc69bb21c8a2
bmijanovic <mijanovic.boki.bojan@gmail.com>
2021-11-27 15:57:47 +0100

    dodata funkcija za parametre po mesecima

---
+++ Barkod2021API_INTelligence/Models/DateCurrencyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barkod2021API_INTelligence.Models
+{
+    public class DateCurrencyModel
+    {
+        public string date { get; set; }
+        public decimal currency { get; set; }
+    }
+}
